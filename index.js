f663ea6a58ea5247683444fe85cd21c1e6e4ae4c
lucas <stick.goal@163.com>
2019-04-26 11:07:23 +0800

    修改端口

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 
 // 创建 application/x-www-form-urlencoded 编码解析
